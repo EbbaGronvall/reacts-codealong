f32552a7627d5433a5a277d4b0af93025f912b55
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-09-08 12:28:59 +0000

    Added nesting components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,10 @@
-
-import './App.css';
-import ConditionalRenderingClass from './components/ConditionalRenderingClass';
-import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
+import "./App.css";
+import NestingComponents from "./components/NestingComponents";
 
 function App() {
-  return (
-    <div className="App">
-      <ConditionalRenderingClass />
-      <ConditionalRenderingFunctional connected={false} />
-    </div>
-  );
+  return <div className="App">
+      <NestingComponents />
+  </div>;
 }
 
 export default App;
