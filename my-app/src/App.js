d25bcd010e9df1ead38eaead6876bfa096b11896
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-09-23 17:32:48 +0000

    Mimiced componentwillunmount

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import ClassCounter from "./components/ClassCounter";
 import UseEffectCounter from "./components/UseEffectCounter";
+import UseEffectCounterContainer from "./components/UseEffectCounterContainer";
 
 function App() {
   return <div className="App">
       {/* <ClassCounter /> */}
-      <UseEffectCounter />
+      {/* <UseEffectCounter /> */}
+      <UseEffectCounterContainer />
   </div>;
 }
 
