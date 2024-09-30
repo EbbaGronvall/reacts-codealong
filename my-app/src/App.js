f7326abbe5a49f39825b16da03d2185e0f1392f7
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-09-30 10:12:18 +0000

    API installed and getrequest method made

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import ClassCounter from "./components/ClassCounter";
 import UseEffectCounter from "./components/UseEffectCounter";
 import UseEffectCounterContainer from "./components/UseEffectCounterContainer";
+import HTTPRequests from "./components/HTTPRequests";
 
 function App() {
   return <div className="App">
       {/* <ClassCounter /> */}
       {/* <UseEffectCounter /> */}
-      <UseEffectCounterContainer />
+      {/* <UseEffectCounterContainer /> */}
+      <HTTPRequests />
   </div>;
 }
 
